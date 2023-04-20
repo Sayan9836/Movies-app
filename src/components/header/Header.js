8285a5da636a0e1592a2e9427a550a86234cb1fb
Sayan9836 <sayanmaity631@gmail.com>
2023-04-20 17:12:50 +0530

    description page

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import "./Header.css"
 import { Link } from "react-router-dom"
@@ -7,26 +6,6 @@ const Header = () => {
         <div className='header'>
             <div className='headerLeft'>
                 <Link to="/"><img className='header__icon' src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png" alt='Loading' /></Link>
-                <Link to="/movie/popular" style={{ textDecoration: "none" }} ><span>Popular</span></Link>
-                <Link to="/movie/top_rated " style={{ textDecoration: "none" }}><span>TopRated</span></Link>
-                <Link to="/movie/upcoming " style={{ textDecoration: "none" }}><span>Upcoming</span></Link>
-            </div>
-
-        </div>
-    )
-}
-
-export default Header
-
-=======
-import React from 'react'
-import "./Header.css"
-import { Link } from "react-router-dom"
-const Header = () => {
-    return (
-        <div className='header'>
-            <div className='headerLeft'>
-                <Link to="/"><img className='header__icon' src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png" alt='' /></Link>
                 <Link to="/movie/movies/popular" style={{ textDecoration: "none" }} ><span>Popular</span></Link>
                 <Link to="/movie/movies/top_rated " style={{ textDecoration: "none" }}><span>TopRated</span></Link>
                 <Link to="/movie/movies/upcoming " style={{ textDecoration: "none" }}><span>Upcoming</span></Link>
@@ -38,4 +17,3 @@ const Header = () => {
 
 export default Header
 
->>>>>>> master
--- src/components/movieList/MovieList.js
+++ src/components/movieList/MovieList.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useEffect, useState } from 'react'
 import Card from '../card/Card'
 import { useParams } from "react-router-dom"
@@ -41,48 +40,4 @@ const MovieList = () => {
 }
 
 export default MovieList
-=======
-import React, { useEffect, useState } from 'react'
-import Card from '../card/Card'
-import { useParams } from "react-router-dom"
-import "./MovieList.css"
 
-const MovieList = () => {
-    const [MovieList, setMovieList] = useState([])
-    const { type } = useParams();
-
-    useEffect(() => {
-        getData()
-        // eslint-disable-next-line
-    }, [])
-
-    useEffect(() => {
-        getData()
-        // eslint-disable-next-line
-    }, [type])
-
-    const getData = () => {
-        fetch(`https://api.themoviedb.org/3/movie/${type ? type : "popular"}?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`)
-            .then(res => res.json())
-            .then(data => setMovieList(data.results))
-            console.log(MovieList);
-    }   
-
-    return (
-
-        <div className='movie__list'>
-            <h2 className='list__title'>{(type ? type : "POPULAR").toUpperCase()}</h2>
-            <div className='list__cards'>
-                {
-                    MovieList.map((movie) => {
-                        return <Card movie={movie} />
-                    })
-                }
-            </div>
-        </div>
-
-    )
-}
-
-export default MovieList
->>>>>>> master
